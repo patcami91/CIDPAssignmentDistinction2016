910617ec53251b7bc544c94c645e93f29ce8854e
Patrick Camilleri <patrcami91@gmail.com>
2016-05-28 01:44:21 +0200

    Fourth update
    obstacles are animated and set to disrupt the player

---
+++ Patrick Camilleri 530791M/Game/Assets/Scripts_Used/MenuButtonScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuButtonScript : MonoBehaviour
+{
+
+
+    public void menutostartgame()
+    {
+        SceneManager.LoadScene("SceneLvl1");
+    }
+   
+
+
+}
