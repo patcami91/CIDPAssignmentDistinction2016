f1131509b1e479e8895fdb3fd8968c5319761e54
Patrick Camilleri <patrcami91@gmail.com>
2016-05-29 08:21:33 +0200

    eight update
    quit and animate it button were added

---
+++ Patrick Camilleri 530791M/Game/Assets/Scripts_Used/AnimateIt.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateIt : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update() { }
+    
+
+        public void ButtonPressed()
+    {
+        GetComponent<Animator>().SetTrigger("menuanimation");
+    }
+
+}
+
+
---
+++ Patrick Camilleri 530791M/Game/Assets/anim.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+
+        //set the trigger when you press space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().SetTrigger("start");
+        }
+        */
+
+    }
+
+
+    public void startanim()
+    {
+        
+        GetComponent<Animator>().SetTrigger("start");
+    }
+
+   
+
+}
