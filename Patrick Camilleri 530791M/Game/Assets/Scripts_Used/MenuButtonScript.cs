bee5005190d600df065015ba5a396cd91392c4e2
Patrick Camilleri <patrcami91@gmail.com>
2016-05-29 03:08:51 +0200

    sixth update
    credit scene, pause, exit to menu and restart level buttons were added

--- Patrick Camilleri 530791M/Game/Assets/Scripts_Used/MenuButtonScript.cs
+++ Patrick Camilleri 530791M/Game/Assets/Scripts_Used/MenuButtonScript.cs
@@ -10,7 +10,43 @@ public class MenuButtonScript : MonoBehaviour
     {
         SceneManager.LoadScene("SceneLvl1");
     }
-   
 
+    public void Instructionstomenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 
+    public void menuToinstructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+	public void creditanimtomenu()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+    public void menutocredit()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void menutoquitgame()
+    {
+        Application.Quit(); 
+    }
+
+    public void RestartLvl1()
+    {
+        SceneManager.LoadScene("SceneLvl1");
+    }
+    public void RestartLvl2()
+    {
+        SceneManager.LoadScene("SceneLvl2");
+    }
+    public void RestartLvl3()
+    {
+        SceneManager.LoadScene("SceneLvl3");
+    }
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
 }
